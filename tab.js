1079c6560322f9974b64a4e2a9cc71a973710e60
sherry q j wang <578331299Qqq.com>
2019-05-28 17:06:13 +0800

    tab jquery demo

--- tab.js
+++ tab.js
@@ -1,7 +1,7 @@
 (function($) {
     // 原型模式
     var Tab = function(tab) {
-        var _this_ = this;
+        var self = this;
         this.tab = tab;
         this.config = {
             "triggerType": "mouseover",
@@ -9,12 +9,81 @@
             "invoke": 1,
             "auto": false
         };
-        this.getConfig();
+        // 扩展参数
+        if (self.getConfig()) {
+           $.extend(this.config, this.getConfig());
+        }
+        // console.log(config);
+        // 保存标签
+        this.tabItems = this.tab.find("ul.tab-nav li");
+        this.contentItem = this.tab.find("div.content-wrap div.content-item");
+        // 绑定事件
+        console.log(this.config);
+        this.tabItems.bind(this.config.triggerType, function() {
+            self.invoke($(this));
+        })
+        // 自动播放
+        if (this.config.auto) {
+            this.lop = 0;
+            this.timer = null;
+            self.autoPlay();
+        }
+        // 设置默认显示的第几个tab
+        if (this.config.invoke) {
+            self.invoke(this.tabItems.eq(this.config.invoke));
+        }
+
     }
     Tab.prototype = {
+        // 获取参数
         getConfig: function() {
-            //var config = this.tab.attr("data-config");
-            console.log(this.tab);
+            var config = this.tab.attr("data-config")
+            if (config && config !== '') {
+                return $.parseJSON(config);
+            } else {
+                return null;
+            }
+        },
+        // 切换标签
+        invoke: function(currentTab) {
+            currentTab.addClass("actived").siblings().removeClass("actived");
+            var index = currentTab.index();
+            var contentItem = this.contentItem;
+            // 切换动画
+            var effect = this.config.effect;
+            if (effect === "default") {
+                contentItem.eq(index).addClass("current").siblings().removeClass("current");
+            } else {
+                contentItem.eq(index).fadeIn().siblings().fadeOut();
+            }
+            if (this.config.auto) {
+                this.lop = index;
+            }
+            
+        },
+        // 自动播放
+        autoPlay () {
+            // 注意this的用法
+            var self = this;
+            tabItems = this.tabItems;
+            tabItemsLength = tabItems.size();
+            config = this.config;
+            // this.timer = window.setInterval(function(){
+            //     // this指向window
+            //     self.lop ++;
+            //     if (self.lop >= tabItemsLength) {
+            //         self.lop = 0;
+            //     }
+            //     tabItems.eq(self.lop).trigger(config.triggerType); 
+            // },config.auto);
+            // 箭头函数改变this的指向
+            this.time = window.setInterval(() => {
+                this.lop ++;
+                if (this.lop >= tabItemsLength) {
+                    this.lop = 0;
+                }
+                tabItems.eq(this.lop).trigger(config.triggerType); 
+            },config.auto)
         }
     }
 
